2b23dccee0397d05a56e15885ce9cc0e8aa7d19b
regfd <neubauersimon@hotmail.com>
2020-01-24 23:24:24 -0500

    JS updates

---
+++ script.js
@@ -0,0 +1,34 @@
+/*function myFunction() {
+    let x = document.getElementById("myBtn").value;
+    document.getElementById("history").innerHTML = x;
+  } */
+
+
+//////// BASIC MATH FUNCTIONS ///////////
+
+function add(a, b)  {
+  return a + b;
+}
+
+function substract(a, b)  {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+//////// BASIC MATH FUNCTIONS END ///////////
+
+
+
+/// OPERATE FUNCTION //
+
+function operate()  {
+
+  
+}
